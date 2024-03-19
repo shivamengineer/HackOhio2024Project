927074b7955727aee443dbb0f255713ee7cf4e56
shivamengineer <34991071+shivamengineer@users.noreply.github.com>
2024-03-18 21:25:31 -0400

    setup basic menu

--- Draw.cpp
+++ Draw.cpp
@@ -17,6 +17,7 @@ void Draw::drawCube(SDL_Renderer* renderer, int *attributes, int *pos, bool *adj
 	int zPos = *(pos + 2);
 	int x = C_X + (xPos * length) - (zPos * length);
 	int y = C_Y - (yPos * length) + (xPos * slope) + (zPos * slope);
+	unsigned char opacity = (unsigned char)*(attributes + 2);
 	float fx = (float)x;
 	float fy = (float)y;
 	float fslope = (float)slope;
@@ -24,14 +25,14 @@ void Draw::drawCube(SDL_Renderer* renderer, int *attributes, int *pos, bool *adj
 	if (!(*adjacent)) {
 		//draw face
 		std::vector<SDL_Vertex> vertex1 = {
-			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx, fy + fslope + flength }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx + flength, fy }, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + fslope + flength }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx + flength, fy }, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
 		};
 		std::vector<SDL_Vertex> vertex2 = {
-			{ SDL_FPoint{fx + flength, fy}, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{ fx, fy + fslope + flength}, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{ fx + flength, fy + flength }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx + flength, fy}, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{ fx, fy + fslope + flength}, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{ fx + flength, fy + flength }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
 		};
 		SDL_RenderGeometry(renderer, nullptr, vertex1.data(), vertex1.size(), nullptr, 0);
 		SDL_RenderGeometry(renderer, nullptr, vertex2.data(), vertex2.size(), nullptr, 0);
@@ -42,14 +43,14 @@ void Draw::drawCube(SDL_Renderer* renderer, int *attributes, int *pos, bool *adj
 	if (!(*(adjacent + 1))) {
 		//draw face
 		std::vector<SDL_Vertex> vertex1 = {
-			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx, fy - fslope }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx - flength, fy }, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy - fslope }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx - flength, fy }, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
 		};
 		std::vector<SDL_Vertex> vertex2 = {
-			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx, fy - fslope }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx + flength, fy }, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy - fslope }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx + flength, fy }, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
 		};
 		SDL_RenderGeometry(renderer, nullptr, vertex1.data(), vertex1.size(), nullptr, 0);
 		SDL_RenderGeometry(renderer, nullptr, vertex2.data(), vertex2.size(), nullptr, 0);
@@ -61,14 +62,14 @@ void Draw::drawCube(SDL_Renderer* renderer, int *attributes, int *pos, bool *adj
 	if (!(*(adjacent + 2))) {
 		//draw face
 		std::vector<SDL_Vertex> vertex1 = {
-			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx, fy + flength + fslope }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{ fx - flength, fy }, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + fslope}, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + flength + fslope }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{ fx - flength, fy }, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
 		};
 		std::vector<SDL_Vertex> vertex2 = {
-			{ SDL_FPoint{fx, fy + flength + fslope}, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx - flength, fy }, SDL_Color{ 50, 50, 50, 255 }, SDL_FPoint{ 0 }, },
-			{ SDL_FPoint{fx - flength, fy + flength }, SDL_Color{ 255, 255, 255, 255 }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx, fy + flength + fslope}, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx - flength, fy }, SDL_Color{ 50, 50, 50, opacity }, SDL_FPoint{ 0 }, },
+			{ SDL_FPoint{fx - flength, fy + flength }, SDL_Color{ 255, 255, 255, opacity }, SDL_FPoint{ 0 }, },
 		};
 		SDL_RenderGeometry(renderer, nullptr, vertex1.data(), vertex1.size(), nullptr, 0);
 		SDL_RenderGeometry(renderer, nullptr, vertex2.data(), vertex1.size(), nullptr, 0);
--- main.cpp
+++ main.cpp
@@ -5,17 +5,21 @@
 #include "Draw.hpp"
 #include "Collide.hpp"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 	TTF_Init();
 
 	int WIDTH = 1000, HEIGHT = 480;
+	int opacityMain = 255;
+	int opacityMenu = 0;
+	bool blur = false;
+	int keyCount = 0;
 
-	SDL_Window *window;
-	SDL_Renderer *renderer;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
 	SDL_DisplayMode dm;
 
-	bool fullscreen = false;
+	bool fullscreen = false, resume = false, exit = false;
 
 	SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
 
@@ -24,8 +28,14 @@ int main(int argc, char *argv[]) {
 		printf("NULL error");
 	}
 
-	SDL_Surface* surface = TTF_RenderText_Solid(roboto, "Hello World!", {255, 255, 255});
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+	SDL_Surface* escSurface = TTF_RenderText_Solid(roboto, "ESC", { 255, 255, 255 });
+	SDL_Texture* escTexture = SDL_CreateTextureFromSurface(renderer, escSurface);
+
+	SDL_Surface* resumeSurface = TTF_RenderText_Solid(roboto, "RESUME", { 255, 255, 255 });
+	SDL_Surface* exitSurface = TTF_RenderText_Solid(roboto, "EXIT", { 255, 255, 255 });
+
+	SDL_Texture* resumeTexture = SDL_CreateTextureFromSurface(renderer, resumeSurface);
+	SDL_Texture* exitTexture = SDL_CreateTextureFromSurface(renderer, exitSurface);
 
 	Draw temp = Draw();
 	Collide collider = Collide();
@@ -38,15 +48,16 @@ int main(int argc, char *argv[]) {
 
 	int posX2, posY2;
 
-	SDL_Rect rect1 = { 20, 20, 200, 30 };
+	SDL_Rect esc = { 10, 10, 40, 20 };
 
 	int* attribs;
-	attribs = (int*)calloc(2, sizeof(int));
+	attribs = (int*)calloc(3, sizeof(int));
 	if (attribs != NULL) {
 		*attribs = slope;
 		*(attribs + 1) = lengthCube;
+		*(attribs + 2) = SDL_ALPHA_OPAQUE;
 	}
-	
+
 	int* pos = (int*)calloc(3, sizeof(int));
 	if (pos != NULL) {
 		*pos = 0;
@@ -74,10 +85,14 @@ int main(int argc, char *argv[]) {
 		return -1;
 	}
 
+	SDL_Rect resumeRect = { WIDTH / 2, (HEIGHT / 2) - 50, 100, 50 };
+	SDL_Rect exitRect = { WIDTH / 2, (HEIGHT / 2) + 50, 100, 50 };
+
 	bool running = true;
 	SDL_Event event;
 
 	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+	SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
 
 	while (running) {
 		if (SDL_PollEvent(&event)) {
@@ -86,7 +101,25 @@ int main(int argc, char *argv[]) {
 			}
 			if (event.type = SDL_KEYDOWN) {
 				switch (event.key.keysym.sym) {
+				case SDLK_j:
+					printf("%d\n", dm.w / 2);
+					break;
 				case SDLK_ESCAPE:
+					keyCount++;
+					if (keyCount == 2) {
+						keyCount = 0;
+						if (!blur) {
+							blur = true;
+							opacityMain = 100;
+							*(attribs + 2) = 100;
+						} else {
+							blur = false;
+							opacityMain = SDL_ALPHA_OPAQUE;
+							*(attribs + 2) = SDL_ALPHA_OPAQUE;
+						}
+					}
+					break;
+				case SDLK_q:
 					running = false;
 					break;
 				case SDLK_f:
@@ -95,7 +128,8 @@ int main(int argc, char *argv[]) {
 						SDL_SetWindowSize(window, dm.w, dm.h + 10);
 						SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
 						fullscreen = true;
-					} else {
+					}
+					else {
 						SDL_RestoreWindow(window);
 						SDL_SetWindowSize(window, WIDTH, HEIGHT);
 						SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
@@ -104,20 +138,33 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 			}
+
+
 			if (event.type = SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
 				SDL_GetMouseState(mouseX, mouseY);
 				int* mouseCoords = (int*)calloc(2, sizeof(int));
 				if (mouseX != NULL && mouseCoords != NULL) {
 					*mouseCoords = *mouseX;
+
 				}
 				if (mouseY != NULL && mouseCoords != NULL) {
 					*(mouseCoords + 1) = *mouseY;
 				}
-				bool collides = collider.mouseObjectCollide(attribs, pos, mouseCoords, adjacent);
+				/*bool collides = collider.mouseObjectCollide(attribs, pos, mouseCoords, adjacent);
 				if (collides) {
-					printf("Collides\n");
+					//printf("Collides\n");
 				} else {
-					printf("Does not collide\n");
+					//printf("Does not collide\n");
+				}*/
+				bool resume = collider.mouseRectCollide(resumeRect, mouseCoords);
+				bool exit = collider.mouseRectCollide(exitRect, mouseCoords);
+				if (resume) {
+					blur = false;
+					opacityMain = SDL_ALPHA_OPAQUE;
+					*(attribs + 2) = SDL_ALPHA_OPAQUE;
+				}
+				if (exit) {
+					running = false;
 				}
 				free(mouseCoords);
 			}
@@ -126,11 +173,21 @@ int main(int argc, char *argv[]) {
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
+		temp.drawCube(renderer, attribs, pos, adjacent);
+
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
-		SDL_RenderCopy(renderer, texture, NULL, &rect1);
+		if (!blur) {
+			SDL_RenderCopy(renderer, escTexture, NULL, &esc);
+			SDL_RenderDrawRect(renderer, &esc);
+		} else {
+			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+			SDL_RenderCopy(renderer, resumeTexture, NULL, &resumeRect);
+			SDL_RenderCopy(renderer, exitTexture, NULL, &exitRect);
+			SDL_RenderDrawRect(renderer, &resumeRect);
+			SDL_RenderDrawRect(renderer, &exitRect);
+		}
 
-		temp.drawCube(renderer, attribs, pos, adjacent);
 		/*temp.drawCube(renderer, attribs, pos2);
 		temp.drawCube(renderer, attribs, pos3);
 		temp.drawCube(renderer, attribs, pos4);
@@ -143,8 +200,13 @@ int main(int argc, char *argv[]) {
 	free(mouseY);
 	free(attribs);
 	free(pos);
-	SDL_FreeSurface(surface);
-	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(escSurface);
+	SDL_FreeSurface(resumeSurface);
+	SDL_FreeSurface(exitSurface);
+
+	SDL_DestroyTexture(escTexture);
+	SDL_DestroyTexture(resumeTexture);
+	SDL_DestroyTexture(exitTexture);
 
 	TTF_Quit();
 	SDL_DestroyWindow(window);
